f12bb87431c58b9bd71dba251e47c33bed084302
Park, minsu <cmwmss59@gmail.com>
2024-08-29 17:41:22 +0900

    Refactor: yaml 파일 local db로 수정

--- src/main/java/discordstudy/calender/domain/member/controller/MemberController.java
+++ src/main/java/discordstudy/calender/domain/member/controller/MemberController.java
@@ -1,4 +1,26 @@
 package discordstudy.calender.domain.member.controller;
 
+import discordstudy.calender.domain.member.entity.Member;
+import discordstudy.calender.domain.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/members")
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<Member> signup(@RequestBody Member member )
+    {
+        Member savedmember = memberService.registermember(member);
+        return ResponseEntity.ok(savedmember);
+        //responseEntity의 body에 savedmember넣기
+    }
 }
--- src/main/java/discordstudy/calender/domain/member/service/MemberService.java
+++ src/main/java/discordstudy/calender/domain/member/service/MemberService.java
@@ -1,4 +1,21 @@
 package discordstudy.calender.domain.member.service;
 
+import discordstudy.calender.domain.member.entity.Member;
+import discordstudy.calender.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class MemberService {
+    //회원가입은 Save를 해야함 !
+    private final MemberRepository memberRepository;
+
+    public Member registermember(Member member)
+
+    {
+        return memberRepository.save(member);
+    }
+
+
 }
