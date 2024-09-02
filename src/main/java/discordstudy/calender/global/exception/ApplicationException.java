b7278e203fa9bcbcd9a29a3b304a10cafda015c8
Galmaeki <kjh09800@gmail.com>
2024-09-02 15:31:06 +0900

    [Feat] 프로젝트 예외 처리용 클래스 추가 - ApplicationException : 프로젝트에서 의도적으로 발생시킨 예외를 핸들링 하기 위한 클래스

---
+++ src/main/java/discordstudy/calender/global/exception/ApplicationException.java
@@ -0,0 +1,24 @@
+package discordstudy.calender.global.exception;
+
+import lombok.Getter;
+
+/**
+ * 여러 라이브러리에서는 RuntimeException 을 상속한 Exception 을 발생시킴
+ * RuntimeException 으로 예외를 처리하게 되면 프로젝트 내 예외와 라이브러리 예외의
+ * 구분이 어려워 질 수 있음<br>
+ * 이를 구분하기 위해 프로젝트 내 예외를 던지기 위한 클래스를 만들어 사용함
+ * */
+@Getter
+public class ApplicationException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    private ApplicationException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public ApplicationException of(ErrorCode errorCode){
+        return new ApplicationException(errorCode);
+    }
+}
