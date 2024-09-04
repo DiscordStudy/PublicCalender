5f6951aae55924ba8c0dbadb9bb10c0842288803
Park, minsu <cmwmss59@gmail.com>
2024-09-04 17:38:26 +0900

    feat: Jwt 토큰을 생성하는 부분인 JwtTokenProvider을 생성했음

---
+++ src/main/java/discordstudy/calender/global/config/jwt/JwtTokenProvider.java
@@ -0,0 +1,44 @@
+package discordstudy.calender.global.config.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Base64;
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class JwtTokenProvider {
+    @Value("${secret_key}")//yaml 파일에 있는 secret key
+    private String secretKey;
+
+    @Value("${ExpirationInMs")//yaml 파일에 있는 expire length
+    private long validityInMilliseconds;
+
+    //Secret Key를 Base64로 인코딩
+    protected void init()
+    {
+        secretKey= Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    public String createToken(String username, List<String> roles) {
+        Claims claims = Jwts.claims().setSubject(username);
+        claims.put("roles", roles);
+
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+
+
+}
